a5fef490208a57610912adfe7fc44a3ffc38ce69
김한식 <dev.imaec@gmail.com>
2025-02-17 21:26:33 +0900

    chore: git ignore에 추가된 폴더/파일 삭제

--- app/src/main/java/info/imdang/const/WebUrlConst.kt
+++
@@ -1,6 +0,0 @@
-package info.imdang.const
-
-const val SERVICE_TERM_URL =
-    "https://principled-cry-2aa.notion.site/54dd2a7ccd5a4c8193e06df782d02119"
-const val PRIVACY_URL = "https://principled-cry-2aa.notion.site/4d557e465d6143a3abc133397966c3d1"
-const val MARKETING_URL = "https://principled-cry-2aa.notion.site/15518cb101a5803ea628dc2f0822711d"
--- app/src/main/java/info/imdang/ui/main/MainBottomBarType.kt
+++
@@ -1,9 +0,0 @@
-package info.imdang.ui.main
-
-import info.imdang.ui.main.home.HOME_SCREEN
-import info.imdang.ui.main.storage.STORAGE_SCREEN
-
-enum class MainBottomBarType(val route: String, val title: String) {
-    HOME(route = HOME_SCREEN, title = "홈"),
-    STORAGE(route = STORAGE_SCREEN, title = "보관함")
-}
