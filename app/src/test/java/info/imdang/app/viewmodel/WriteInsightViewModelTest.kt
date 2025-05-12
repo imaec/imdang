37c3ffddf72dbc7a253c419a2d402cf3b1bac045
김한식 <dev.imaec@gmail.com>
2025-05-12 23:49:09 +0900

    test: WriteInsightViewModel init 테스트 구현

---
+++ app/src/test/java/info/imdang/app/viewmodel/WriteInsightViewModelTest.kt
@@ -0,0 +1,102 @@
+package info.imdang.app.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import info.imdang.app.ui.insight.write.WriteInsightViewModel
+import info.imdang.domain.model.common.AddressDto
+import info.imdang.domain.model.insight.ApartmentComplexDto
+import info.imdang.domain.model.insight.InsightDetailDto
+import info.imdang.domain.usecase.insight.GetInsightDetailUseCase
+import info.imdang.domain.usecase.insight.UpdateInsightUseCase
+import info.imdang.domain.usecase.insight.WriteInsightUseCase
+import io.mockk.coEvery
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class WriteInsightViewModelTest {
+
+    private lateinit var getInsightDetailUseCase: GetInsightDetailUseCase
+    private lateinit var writeInsightUseCase: WriteInsightUseCase
+    private lateinit var updateInsightUseCase: UpdateInsightUseCase
+    private lateinit var viewModel: WriteInsightViewModel
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(StandardTestDispatcher())
+        getInsightDetailUseCase = mockk()
+        writeInsightUseCase = mockk()
+        updateInsightUseCase = mockk()
+
+        viewModel = WriteInsightViewModel(
+            savedStateHandle = SavedStateHandle().apply {
+                set("insightId", "testInsightId")
+            },
+            getInsightDetailUseCase = getInsightDetailUseCase,
+            writeInsightUseCase = writeInsightUseCase,
+            updateInsightUseCase = updateInsightUseCase
+        )
+    }
+
+    @Test
+    fun `WriteInsightViewModel 생성 시 insightId가 null이 아니면 인사이트 상세 조회`() =
+        runTest(UnconfinedTestDispatcher()) {
+            // given
+            val insightId = "testInsightId"
+            val insight = InsightDetailDto(
+                memberId = "testMemberId",
+                memberNickname = "testMemberNickname",
+                insightId = "testInsightId",
+                snapshotId = 1L,
+                mainImage = "testMainImage",
+                title = "testTitle",
+                address = AddressDto(
+                    siDo = "testSido",
+                    siGunGu = "testSigungu",
+                    eupMyeonDong = "testEupmyeondong",
+                    roadName = "testRoadname",
+                    buildingNumber = "testBuildingnumber",
+                    detail = "testDetail",
+                    latitude = 1.0,
+                    longitude = 2.0
+                ),
+                apartmentComplex = ApartmentComplexDto(name = "testComplexName"),
+                visitAt = "2025-05-12",
+                visitTimes = listOf("testVisitTime"),
+                visitMethods = listOf("testVisitMethod"),
+                access = "testAccess",
+                summary = "testSummary",
+                infra = null,
+                complexEnvironment = null,
+                complexFacility = null,
+                favorableNews = null,
+                recommended = true,
+                accused = false,
+                recommendedCount = 1,
+                accusedCount = null,
+                viewCount = 1,
+                score = 1,
+                createdAt = "testCreatedAt",
+                createdByMe = true,
+                exchangeRequestStatus = null,
+                exchangeRequestCreatedByMe = null,
+                exchangeRequestId = null
+            )
+
+            coEvery { getInsightDetailUseCase(insightId, any()) } returns insight
+
+            // when
+            delay(100)
+
+            // then
+            assertEquals(insightId, viewModel.insightId)
+        }
+}
